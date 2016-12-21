a44ffa32fbc20a6104a2d8c61128022d17cd5395
Warley Ferreira Silva <warley.ferreira@gmail.com>
2016-12-21 19:31:50 -0200

    Inclusão dos projetos referentes às camadas da aplicação.

--- LocaCarro/LocaCarro.Domain/Properties/AssemblyInfo.cs
+++ LocaCarro/LocaCarro.Domain/Properties/AssemblyInfo.cs
@@ -2,34 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LocaCarro.Presentation.Web")]
+[assembly: AssemblyTitle("LocaCarro.Domain")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LocaCarro.Presentation.Web")]
+[assembly: AssemblyProduct("LocaCarro.Domain")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0e442980-06a3-4b87-931d-8f3439e6b222")]
+[assembly: Guid("c7d9c3aa-24d7-4170-90b4-5fcae6d269ec")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
